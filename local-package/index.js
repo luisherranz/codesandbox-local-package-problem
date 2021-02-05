f41b21f6869cac3d457bad8ffa6372ca21cdabab
Luis Herranz <luisherranz@gmail.com>
2021-02-05 12:40:43 +0100

    Initial commit

---
+++ local-package/index.js
@@ -0,0 +1,1 @@
+module.exports = () => console.log("I am a local package");
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const localPackage = require("local-package");
+
+localPackage();
+
+//create a server object:
+http
+  .createServer(function (req, res) {
+    res.write("Hello World!"); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080
